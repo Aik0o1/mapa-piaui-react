b9e600b91549df60e8242de60c0fe31fea9c0eb1
cibellemc <cibellemaciel02@gmail.com>
2025-01-03 12:18:43 -0300

    feat: estado de mes e ano recebidos pelo app

--- src/App.jsx
+++ src/App.jsx
@@ -7,27 +7,26 @@ import Filtros from './components/main/Filtros'
 
 function MainContent() {
   const [cidade, setCidade] = useState(null);
-  const [mes, setMes] = useState([
-    'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
-    'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
-  ]);
+  const [mes, setMes] = useState(null);
   const [ano, setAno] = useState();
 
   // Atualiza estado quando nova cidade é clicada/selecionada
   const handleCidade = (cidade) => {
     setCidade(cidade);
-    console.log("Cidade selecionada no App:", cidade);
+    // console.log("Cidade selecionada no App:", cidade);
   };
 
   // Atualiza estado quando um mês é selecionado
   const handleMes = (mes) => {
     setMes(mes);
-    console.log(mes);
+    console.log("Mês selecionado no App:", mes);
   };
 
   // Atualiza estado quando o ano é selecionado
   const handleAnoSelecionado = (ano) => {
     setAno(ano);
+    console.log("Ano selecionado no App:", ano);
+
   };
 
   return (
@@ -38,14 +37,14 @@ function MainContent() {
       <Filtros
         onCidadeSelecionada={handleCidade}
         selectedCity={cidade}
-        meses={mes}
         onMesSelecionado={handleMes}
-        selectedMonth={mes}
+        onAnoSelecionado={handleAnoSelecionado}
       />
 
       <div className="conteudo">
         <PiauiMapa
           onCidadeSelecionada={handleCidade}
+          selectedCity={cidade}
         />
 
         <Lista onCidadeSelecionada={cidade} />
--- src/components/main/Filtros.jsx
+++ src/components/main/Filtros.jsx
@@ -1,46 +1,63 @@
 import { ComboboxCidades } from '../ui/combobox';
-import React from 'react';
+import { useState } from 'react';
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
 } from "@/components/ui/select";
 
 export default function Filtros(props) {
-  const handleCidadeSelect = (cidade) => {
-    props.onCidadeSelecionada(cidade); // Envia a cidade para o pai
-    console.log("Cidade selecionada no Filtros:", cidade);
-  };
+    const [meses, setMes] = useState([
+        'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+        'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
+    ]);
 
-  return (
-    <div className="filtros">
-      <p>Selecione um município ou período</p>
-      <div className="selecao">
-        <ComboboxCidades onCidadeSelect={handleCidadeSelect} />
-        <Select className="mes">
-          <SelectTrigger>
-            <SelectValue placeholder="Selecione um mês" />
-          </SelectTrigger>
-          <SelectContent>
-            {props.meses.map((mes, index) => (
-              <SelectItem key={index} value={mes}>
-                {mes}
-              </SelectItem>
-            ))}
-          </SelectContent>
-        </Select>
+    const handleCidadeSelect = (cidade) => {
+        props.onCidadeSelecionada(cidade); // Envia a cidade para o pai
+        // console.log("Cidade selecionada no Filtros:", cidade);
+    };
 
-        <Select className="ano">
-          <SelectTrigger>
-            <SelectValue placeholder="Selecione um ano" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="2024">2024</SelectItem>
-          </SelectContent>
-        </Select>
-      </div>
-    </div>
-  );
+    const handleMesSelect = (mes) => {
+        props.onMesSelecionado(mes); // Envia o mês para o pai
+        // console.log("Mês selecionado no Filtros:", mes);
+    };
+
+    const handleAnoSelect = (ano) => {
+        props.onAnoSelecionado(ano); // Envia o ano para o pai
+        // console.log("Ano selecionado no Filtros:", ano);
+    };
+
+    return (
+        <div className="filtros">
+            <p>Selecione um município ou período</p>
+            <div className="selecao">
+                <ComboboxCidades onCidadeSelect={handleCidadeSelect} />
+                <Select className="mes" onValueChange={handleMesSelect}>
+                    <SelectTrigger>
+                        <SelectValue placeholder="Selecione um mês" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        {meses.map((mes, index) => (
+                            <SelectItem
+                                key={index}
+                                value={mes}
+                            >
+                                {mes}
+                            </SelectItem>
+                        ))}
+                    </SelectContent>
+                </Select>
+                <Select className="ano" onValueChange={handleAnoSelect}>
+                    <SelectTrigger>
+                        <SelectValue placeholder="Selecione um ano" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectItem value="2024">2024</SelectItem>
+                    </SelectContent>
+                </Select>
+            </div>
+        </div>
+    );
 }
