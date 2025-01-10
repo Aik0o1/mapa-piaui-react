b0a285cb645b82654c48ef19b02a1487d0efd620
Victor Sena <107523354+Aik0o1@users.noreply.github.com>
2025-01-10 12:52:28 -0300

    responsividade

--- src/components/main/Header.jsx
+++ src/components/main/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-      <header className="bg-white shadow-md py-4 px-6 flex items-center justify-between">
+      <header className="header bg-white shadow-md py-4 px-6 flex items-center justify-between">
         <div className="flex items-center justify-center gap-4">
           <img 
             src="https://portal.pi.gov.br/jucepi/wp-content/uploads/sites/47/2023/03/jucepi_logo-768x177.jpg" 
