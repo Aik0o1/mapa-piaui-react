148e45ce8383b5bbf06770da8a8385599389fba3
cibellemc <cibellemaciel02@gmail.com>
2025-07-24 13:34:33 -0300

    feat: filtro de ano de 2019 a 2025

--- front/src/components/main/Filtros.jsx
+++ front/src/components/main/Filtros.jsx
@@ -1,132 +1,157 @@
-
-import { useEffect } from 'react';
-import { ComboboxCidades } from '../ui/combobox';
-import { useState } from 'react';
-import * as d3 from 'd3';
+import { useEffect } from "react";
+import { ComboboxCidades } from "../ui/combobox";
+import { useState } from "react";
+import * as d3 from "d3";
 import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
 } from "@/components/ui/select";
-import { Button } from '../ui/button';
+import { Button } from "../ui/button";
 
 export default function Filtros(props) {
-    const [meses, setMes] = useState([
-        'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
-        'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
-    ]);
+  const [meses, setMes] = useState([
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro",
+  ]);
 
-    const mesesDict = {'01':'Janeiro', '02':'Fevereiro', '03':'Março', '04':'Abril', '05':'Maio', '06':'Junho',
-        '07':'Julho', '08':'Agosto', '09':'Setembro', "10":'Outubro', "11":'Novembro', '12':'Dezembro'}
+  const anos = [];
 
-    const [selectedMes, setSelectedMes] = useState(props.selectedMonth);
-    const [selectedAno, setSelectedAno] = useState(props.selectedYear);
-    const [selectedCidade, setSelectedCidade] = useState(props.cidadeSelecionada);
-    const [dataRecente, setDataRecente] = useState()
+  const mesesDict = {
+    "01": "Janeiro",
+    "02": "Fevereiro",
+    "03": "Março",
+    "04": "Abril",
+    "05": "Maio",
+    "06": "Junho",
+    "07": "Julho",
+    "08": "Agosto",
+    "09": "Setembro",
+    10: "Outubro",
+    11: "Novembro",
+    12: "Dezembro",
+  };
 
+  const [selectedMes, setSelectedMes] = useState(props.selectedMonth);
+  const [selectedAno, setSelectedAno] = useState(props.selectedYear);
+  const [selectedCidade, setSelectedCidade] = useState(props.cidadeSelecionada);
+  const [dataRecente, setDataRecente] = useState();
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const url = "http://localhost:5000/data_recente"
-                const response = await fetch(url);
-                const data = await response.json();
-                setDataRecente(data);
-                // console.log(data)
-            } catch (error) {
-                console.error("Erro ao buscar dados do servidor:", error);
-              }
-        }
-
-        fetchData();
-      }, []);
-
-    const handleCidadeSelect = (cidade) => {
-        props.onCidadeSelecionada(cidade);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const url = "http://localhost:5000/data_recente";
+        const response = await fetch(url);
+        const data = await response.json();
+        setDataRecente(data);
+        // console.log(data)
+      } catch (error) {
+        console.error("Erro ao buscar dados do servidor:", error);
+      }
     };
 
-    const handleMesSelect = (mes) => {
-        setSelectedMes(mes);
-        props.onMesSelecionado(mes);
-    };
+    fetchData();
+  }, []);
 
-    const handleAnoSelect = (ano) => {
-        setSelectedAno(ano);
-        props.onAnoSelecionado(ano);
-    };
+  const handleCidadeSelect = (cidade) => {
+    props.onCidadeSelecionada(cidade);
+  };
 
+  const handleMesSelect = (mes) => {
+    setSelectedMes(mes);
+    props.onMesSelecionado(mes);
+  };
 
-    const highlightCityOnMap = () => {
-        const svg = d3.select("#map");
-        
-        svg.selectAll(".city")
-          .classed("selected", false)
-          .classed("no-selected", false)
-          .transition()
-          .duration(300)
-        
-      };
+  const handleAnoSelect = (ano) => {
+    setSelectedAno(ano);
+    props.onAnoSelecionado(ano);
+  };
 
-    const limparFiltros = () => {
-        setSelectedMes(mesesDict[dataRecente.mes]);
-        setSelectedAno(dataRecente.ano);
-        setSelectedCidade("")
+  const highlightCityOnMap = () => {
+    const svg = d3.select("#map");
 
-        props.onMesSelecionado(mesesDict[dataRecente.mes]);
-        props.onAnoSelecionado(dataRecente.ano);
-        props.onCidadeSelecionada({nome:"Selecione um município", id:""});
-        highlightCityOnMap()
-    }
+    svg
+      .selectAll(".city")
+      .classed("selected", false)
+      .classed("no-selected", false)
+      .transition()
+      .duration(300);
+  };
 
+  const limparFiltros = () => {
+    setSelectedMes(mesesDict[dataRecente.mes]);
+    setSelectedAno(dataRecente.ano);
+    setSelectedCidade("");
 
-    return (
-        <div className="filtros text-[#034ea2]">
-            <div className="selecao">
-            <p>Selecione um município ou período</p>
-                <ComboboxCidades onCidadeSelect={handleCidadeSelect} cidadeSelecionada={props.cidadeSelecionada} />
-                <Select 
-                    className="anoEscolha" 
-                    onValueChange={handleAnoSelect}
-                    value={selectedAno}
-                >
-                    <SelectTrigger className="anoEscolha w-[250px]">
-                        <SelectValue placeholder="Selecione um ano" />
-                    </SelectTrigger>
-                    <SelectContent>
-                        <SelectItem value="2024">2024</SelectItem>
-                        <SelectItem value="2025">2025</SelectItem>
+    props.onMesSelecionado(mesesDict[dataRecente.mes]);
+    props.onAnoSelecionado(dataRecente.ano);
+    props.onCidadeSelecionada({ nome: "Selecione um município", id: "" });
+    highlightCityOnMap();
+  };
 
-                    </SelectContent>
-                </Select>
-                <Select 
-                    className="mes" 
-                    onValueChange={handleMesSelect}
-                    value={selectedMes}
-                >
-                    <SelectTrigger className="mesEscolha w-[250px]">
-                        <SelectValue placeholder="Selecione um mês" />
-                    </SelectTrigger>
-                    <SelectContent>
-                        {meses.map((mes, index) => (
-                            <SelectItem
-                                key={index}
-                                value={mes}
-                            >
-                                {mes}
-                            </SelectItem>
-                        ))}
-                    </SelectContent>
-                </Select>
-                <Button
-                    className="limpar-filtros w-[250px]"
-                    variant="outline"
-                    onClick={limparFiltros}
-                >
-                    Limpar Filtros
-                </Button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="filtros text-[#034ea2]">
+      <div className="selecao">
+        <p>Selecione um município ou período</p>
+        <ComboboxCidades
+          onCidadeSelect={handleCidadeSelect}
+          cidadeSelecionada={props.cidadeSelecionada}
+        />
+        <Select
+          className="anoEscolha"
+          onValueChange={handleAnoSelect}
+          value={selectedAno}
+        >
+          <SelectTrigger className="anoEscolha w-[250px]">
+            <SelectValue placeholder="Selecione um ano" />
+          </SelectTrigger>
+          <SelectContent>
+            {Array.from({ length: 7 }, (_, i) => {
+              const ano = 2019 + i;
+              return (
+                <SelectItem key={ano} value={ano.toString()}>
+                  {ano}
+                </SelectItem>
+              );
+            })}
+          </SelectContent>
+        </Select>
+        <Select
+          className="mes"
+          onValueChange={handleMesSelect}
+          value={selectedMes}
+        >
+          <SelectTrigger className="mesEscolha w-[250px]">
+            <SelectValue placeholder="Selecione um mês" />
+          </SelectTrigger>
+          <SelectContent>
+            {meses.map((mes, index) => (
+              <SelectItem key={index} value={mes}>
+                {mes}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+        <Button
+          className="limpar-filtros w-[250px]"
+          variant="outline"
+          onClick={limparFiltros}
+        >
+          Limpar Filtros
+        </Button>
+      </div>
+    </div>
+  );
 }
