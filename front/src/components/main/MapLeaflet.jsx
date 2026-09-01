50dcc020594760b8f370683d47ec7babda6fbef4
Aik0o1 <anesrotciv@gmail.com>
2026-09-01 13:17:46 -0300

    fix: Adiciona API key do CARTO na URL do tile layer do mapa

--- front/src/components/main/MapLeaflet.jsx
+++ front/src/components/main/MapLeaflet.jsx
@@ -189,7 +189,7 @@ const MapLeaflet = ({ onCidadeSelecionada, cidades, cidadeSelecionada }) => {
         >
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-                url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+                url={`https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png?key=${import.meta.env.VITE_CARTO_API_KEY}`}
             />
 
             <MapAutoZoom cidadeSelecionada={cidadeSelecionada} geoJsonData={geoJsonData} />
